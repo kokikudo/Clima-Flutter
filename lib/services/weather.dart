15ae8a163d26b97dea814b9fa96e0efa716b895c
kokikudo <massanmesu@icloud.com>
2021-07-12 12:09:03 +0900

    キーボードが出るとき、たまにエラーが出る。原因調査中。

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,28 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = 'f9e51e2a1c21397616df28d563c4420b';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    Networking networking = Networking(
+        url: '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    var weatherData = await networking.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    Networking networking = Networking(
+        url:
+            '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+    var weatherData = await networking.getData();
+    return weatherData;
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
@@ -16,6 +40,26 @@ class WeatherModel {
       return '☁️';
     } else {
       return '🤷‍';
+    }
+  }
+
+  String getBackground(int condition) {
+    if (condition < 300) {
+      return 'weather.001.jpeg';
+    } else if (condition < 400) {
+      return 'weather.002.jpeg';
+    } else if (condition < 600) {
+      return 'weather.002.jpeg';
+    } else if (condition < 700) {
+      return 'weather.003.jpeg';
+    } else if (condition < 800) {
+      return 'weather.004.jpeg';
+    } else if (condition == 800) {
+      return 'weather.005.jpeg';
+    } else if (condition <= 804) {
+      return 'weather.006.jpeg';
+    } else {
+      return 'weather.007.jpeg';
     }
   }
 
